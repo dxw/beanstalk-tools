0f722c72bdc508ad86cb2e7290df64ed2bed0a84
Tom Adams <tom@dxw.com>
2018-03-21 10:00:49 -0400

    Add bst-export command

---
+++ cmd/bst-export/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/kr/beanstalk"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: bst-export hostname:11300 tubename")
+	}
+
+	host := os.Args[1]
+	tubeName := os.Args[2]
+
+	conn, err := beanstalk.Dial("tcp", host)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	tubeSet := beanstalk.NewTubeSet(conn, tubeName)
+
+	for {
+		_, data, err := tubeSet.Reserve(1 * time.Second)
+		if e, ok := err.(beanstalk.ConnError); ok && e.Err == beanstalk.ErrTimeout {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("---")
+		fmt.Println(string(data))
+	}
+}
